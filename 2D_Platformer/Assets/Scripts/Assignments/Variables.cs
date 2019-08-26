02940610bd861199bab79444359b54acd1778093
Preston Powell <preston.powell@uvu.edu>
2019-08-26 17:11:07 -0600

    Added variables

---
+++ 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string name = "Preston";
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(name);
+    }
+
+    
+}
