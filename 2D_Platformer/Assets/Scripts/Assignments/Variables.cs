66e9f2e597cc26619f9d2d063984c1b4422085df
Preston Powell <preston.powell@uvu.edu>
2019-08-28 17:04:00 -0600

    Added more variables

--- 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
+++ 2D_Platformer/Assets/Scripts/Assignments/Variables.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    string name = "Preston";
+    string firstName = "Preston";
+    int age = 40;
+    float height = 6.3f;
+    bool married = true;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(name);
+        Debug.Log("First Name: " + firstName);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Married: " + married);
     }
 
     
