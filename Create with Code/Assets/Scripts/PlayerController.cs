7c9a6dde51d25117e2f6608146745a4b0e871ea7
Preston Powell <preston.powell@uvu.edu>
2019-09-18 17:04:24 -0600

    Input controls forward, left & right

--- Create with Code/Assets/Scripts/PlayerController.cs
+++ Create with Code/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   // Game private variables
+   private float speed = 20f;
+   private float turnSpeed = 15f;
+   private float horizontalInput;
+   private float forwardInput; 
+   
     // Update is called once per frame
     void Update()
     {
-        //This code makes the vehicle move
-       
-       transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       // This controls the movement of the vehicle 
+       horizontalInput = Input.GetAxis("Horizontal");
+       forwardInput = Input.GetAxis("Vertical");
+       // This makes the vehicle move horizontal and vertical
+       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
