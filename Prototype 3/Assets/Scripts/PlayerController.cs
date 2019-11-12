66f3989b85e97d55f616844cf3119c40bacd91d9
Preston Powell <preston.powell@uvu.edu>
2019-11-11 17:01:46 -0700

    Added Prototype 4

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    void Awake()
+    {
+        //Comment
+    }
     // Start is called before the first frame update
     void Start()
     {
